cb84197ee3a87a2c997c8a8570e30cfcbd7d4cc3
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-21 12:47:01 -0500

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-class Square():
+class Square:
+    """Basic square class #4"""
+
     def __init__(self, size=0):
-        self.__size = size
+        """Initialize the square"""
+        self.size = size
 
     @property
     def size(self):
+        """Size Getter"""
         return self.__size
 
-    def area(self):
-        return self.__size ** 2
-
     @size.setter
-    def size(self, size_s):
-        if type(size_s) != int:
-            raise TypeError("size must be an integer")
-        if size_s < 0:
-            raise ValueError("size must be >= 0")
-        self.__size = size_s
+    def size(self, value):
+        """Size Setter"""
+        if type(value) is not int:
+            raise TypeError('size must be an integer')
+        elif value < 0:
+            raise ValueError('size must be >= 0')
+        else:
+            self.__size = value
+
+    def area(self):
+        """Calculates the area"""
+        return self.__size ** 2
