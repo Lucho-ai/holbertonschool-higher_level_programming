05b62374743b4e821838c0d45566fa59092e5eb6
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-25 20:35:08 -0500

    Update 4-rectangle.py

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python3
-class Rectangle():
+"""class #5 for rectangle project"""
+
+
+class Rectangle:
+    """rectangle object"""
+
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.width = width
+        self.height = height
 
     @property
     def width(self):
         return self.__width
 
-    @property
-    def height(self):
-        return self.__height
-
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
+    @property
+    def height(self):
+        return self.__height
+
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -32,17 +37,14 @@ class Rectangle():
         return self.width * self.height
 
     def perimeter(self):
-        if self.width == 0 or self.height == 0:
+        if self.area() == 0:
             return 0
-        return (self.width + self.height) * 2
+        return 2 * self.width + 2 * self.height
 
     def __str__(self):
-        if self.height == 0 or self.width == 0:
-            return ""
-        tmp = []
-        for _ in range(self.height):
-            tmp.append('#' * self.width)
-        return "\n".join(tmp)
+        if self.area() == 0:
+            return ''
+        return '\n'.join(['#' * self.width] * self.height)
 
     def __repr__(self):
-        return "Rectangle({}, {})".format(self.width, self.height)
+        return ("Rectangle({:d}, {:d})".format(self.width, self.height))
