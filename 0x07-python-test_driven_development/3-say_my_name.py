234ddcc716bed0b49c6857180a46e4c0765ae9b1
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-29 20:16:47 -0500

    Update 3-say_my_name.py

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,28 +1,23 @@
 #!/usr/bin/python3
-"""
-Prints a name
-prints My name is <first name> <last name>
-Return: none
+"""Task 2 - say my name
+This is our function to be tested
 """
 
 
-def say_my_name(first_name, last_name=""):
-    """Programt that prints a name
-    prints My name is <first name> <last name>
-    Return: none"""
-
-    fn = 0
-    ln = 0
-    if type(first_name) is str:
-        fn = 1
-    elif not isinstance(first_name, str):
-        raise TypeError("first_name must be a string")
-    if type(last_name) is str:
-        ln = 1
-    elif not isinstance(last_name, str):
-        raise TypeError("last_name must be a string")
-
-    if fn == 1 and ln == 0:
-        print("My name is {} ".format(first_name))
-    else:
-        print("My name is {} {}".format(first_name, last_name))
+def say_my_name(first_name, last_name=''):
+    """Prints the full name in a specific format
+    Args:
+        first_name (string): first parameter
+        last_name (string): default empty
+    Returns:
+        Returns nothing
+    Raises:
+        TypeError: first and last name must be a string
+    """
+    te1 = "first_name must be a string"
+    te2 = "last_name must be a string"
+    if type(first_name) is not str:
+        raise TypeError(te1)
+    if type(last_name) is not str:
+        raise TypeError(te2)
+    print("My name is {} {}".format(first_name, last_name))
