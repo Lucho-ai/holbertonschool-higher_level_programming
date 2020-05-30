d339bf7734d17f2dddf02bfe1a22cbbcb6957828
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-29 20:17:32 -0500

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,23 +1,20 @@
 #!/usr/bin/python3
-"""
-Prints a text with 2 new lines after each coincidence
-coincidence characters: ., ? and :
-Return: none
+"""Task 4 - add newlines function
+This is our function to be tested
 """
 
 
-def text_indentation(text):
-    """Programt that prints a text with 2 new lines after each coincidence
-    coincidence characters: ., ? and :
-    Return: none"""
-
-    if isinstance(text, str):
-        coincidence = ['.', '?', ':']
-        launch = 0
-        for i, x in enumerate(text):
-            if x in coincidence:
-                print(text[launch:i + 1].strip() + "\n")
-                launch = i + 1
-        print(text[launch:].strip(), end='')
-    if not isinstance(text, str):
+def text_indentation(text=''):
+    """Adds 2 newlines following specific characters
+    Args:
+        text (str):: first parameter
+    Returns:
+        Returns nothing
+    Raises:
+        TypeError: text must be a string
+    """
+    if type(text) is not str:
         raise TypeError('text must be a string')
+    for c in ".?:":
+        text = text.replace(c, c + "\n\n")
+    print('\n'.join(t.strip() for t in text.split('\n')), end='')
