931fa0a7b9b9327df8f8d3e19c285bbdab7e31f0
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-29 21:35:39 -0500

    Update 6-max_integer_test.py

--- 0x07-python-test_driven_development/test/6-max_integer_test.py
+++ 0x07-python-test_driven_development/test/6-max_integer_test.py
@@ -6,33 +6,30 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """Programt Unittest for max_integer
-    test max_integer
-    Return: none"""
 
-    te_m = "'>' not supported between instances of 'str' and 'int'"
+    def setUp(self):
+        pass
 
-    def test_std_behavior(self):
-        a = [1, 2, 3]
-        self.assertEqual(max_integer(a), 3)
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
 
-    def test_negatives(self):
-        self.assertEqual(max_integer([-3, 5, -2]), 5)
+    def test_single_item(self):
+        self.assertEqual(max_integer([1]), 1)
 
-    def test_floats(self):
-        self.assertEqual(max_integer([2.94, 1, 8.493]), 8.493)
+    def test_negative_item(self):
+        self.assertEqual(max_integer([-1, 0]), 0)
 
-    def test_multi_lists(self):
-        a = [1, 2, 3]
-        b = [1, 9, 3]
-        c = [4, 2, 1]
-        d = [a, b, c]
-        self.assertEqual(max_integer(d), [4, 2, 1])
+    def test_repeat_items(self):
+        self.assertEqual(max_integer([1, -1, 0, 0, 1, -1]), 1)
 
-    def test_tuples(self):
-        self.assertEqual(max_integer((8, 3, 12)), 12)
+    def test_all_same(self):
+        self.assertEqual(max_integer([0, 0, 0]), 0)
 
-    def test_wrong_type(self):
-        a = [1, 2, 'd', 3]
-        with self.assertRaises(TypeError, msg=self.te_m):
-            max_integer(a)
+    def test_descending_list(self):
+        self.assertEqual(max_integer([5, 4, 3, 2, 1]), 5)
+
+    def test_middle(self):
+        self.assertEqual(max_integer([0, 1, 2, 1, -1]), 2)
+
+if __name__ == '__main__':
+    unittest.testmaxinteger()
