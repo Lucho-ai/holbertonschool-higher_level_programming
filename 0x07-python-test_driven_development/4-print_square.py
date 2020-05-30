e0cdfd358467c74be53256e179f95225d2957eae
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-05-29 20:17:11 -0500

    Update 4-print_square.py

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
-"""
-Prints a square
-Print with '#'
-Return: none
+"""Task 3 - prints a square
+This is our function to be tested
 """
 
 
 def print_square(size):
-    """Programt that prints a square
-    Print with '#'
-    Return: none"""
-
-    if isinstance(size, int):
-        for i in range(size):
-            for j in range(size):
-                print('#', end="")
-            print()
-    if not isinstance(size, int):
+    """Prints a square made with the char #
+    Args:
+        size (int):: first parameter
+    Returns:
+        Returns nothing
+    Raises:
+        TypeError: size must be an integer
+        ValueError: size must be an integer
+    """
+    if type(size) is not int:
         raise TypeError('size must be an integer')
     if size < 0:
         raise ValueError('size must be >= 0')
+    for r in range(size):
+        print("#" * size)
