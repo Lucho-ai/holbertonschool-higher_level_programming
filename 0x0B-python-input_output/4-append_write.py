1ede864948866b778cf0bb0c1ebea3ddd7f843fc
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-06-03 19:09:52 -0500

    Create 4-append_write.py

---
+++ 0x0B-python-input_output/4-append_write.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""4_append_write"""
+
+def append_write(filename="", text=""):
+    """append_write function"""
+    with open(filename, "a") as f:
+        return f.write(text)
