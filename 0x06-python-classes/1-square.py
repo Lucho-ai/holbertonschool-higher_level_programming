53e2fd4d5bc24a609034f2172ae7c5fcd0bfa939
Lucho-ai <56923543+Lucho-ai@users.noreply.github.com>
2020-09-22 21:30:29 -0500

    Update 1-square.py

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
-class Square:
-    def __init__(self, size=0):
+class Square():
+    """Square class."""
+
+    def __init__(self, size):
+        """Getting class."""
         self.__size = size
