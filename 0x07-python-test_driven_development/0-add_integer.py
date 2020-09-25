a2bcae9f07f307d299b7beebfcbbe2a1d1b3f30d
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-09-25 19:00:16 +0000

    Intege

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Programt to add to number
+(a) - (b) must be and integer or float type
+return the sum of a + b
+"""
+
+
+def add_integer(a, b=98):
+    """Programt to add to number
+    (a) + (b) must be and integer or float type
+    return the sum of a + b"""
+
+    if type(a) is float or type(a) is int:
+        a = int(a)
+    else:
+        raise TypeError('a must be an integer')
+    if type(b) is float or type(b) is int:
+        b = int(b)
+    else:
+        raise TypeError('b must be an integer')
+    return a + b
