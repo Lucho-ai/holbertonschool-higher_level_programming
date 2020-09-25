25e07d8c353def66e3581eafa096b5661630a4bf
Lucho-ai <luchoalejomarin1999@gmail.com>
2020-09-25 19:38:36 +0000

    Upload

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+Programt that prints a square
+Print with '#'
+Return: none
+"""
+
+
+def print_square(size):
+    """Programt that prints a square
+    Print with '#'
+    Return: none"""
+
+    if isinstance(size, int):
+        for i in range(size):
+            for j in range(size):
+                print('#', end="")
+            print()
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    if size < 0:
+        raise ValueError('size must be >= 0')
